47599b038a61ff51273827fb464d3cffc9061960
pan0shao <304443042@qq.com>
2019-05-06 22:48:45 +0800

    aaa

--- Todo/app/views.py
+++ Todo/app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from app.models import Todo
-from flask import render_template
+from flask import render_template,request
 @app.route('/')
 def HelloWorld():
     return 'Hello World!'
@@ -11,3 +11,9 @@ def todo():
     #return render_template( "index.html",site_name="测试继承" )
     return render_template( "index.html",site_name="测试继承", todos = todos )
 
+@app.route('/add', methods=['POST',])
+def add():
+    content = form[content]
+    todo = Todo(content=content)
+    db.session.add(todo)
+    db.session.commit()
